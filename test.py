e3c8f00255dfb49a10a80a177f7c88b13e2add96
DileepKottilil <dileepkiitm@gmail.com>
2021-06-28 13:50:59 +0800

    DBRplot correction1

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
-
 import multiopti as mop
 
 mo = mop.multiopti(1,3,200) #create an instance of class multiopt
 mo.ref_indx(source = 'theory')
 mo.EM()
-mo.DBR(Bragg = 555, mode = 1, air_n = 1, DBR_per_up = 6,DBR_per_bot = 2,
-              lr1_n = 1.5, lr2_n = 2.5, cav_n = 3, lr4_n = 2.5, lr5_n = 1.5, sub_n = 1.5,
-              exc_num = 3, exc_thick = 10)
+mo.DBR(Bragg = 555, mode = 10, air_n = 1.2, DBR_per_up = 3,DBR_per_bot = 2,
+              lr1_n = 1.5, lr2_n = 2.5, cav_n = 3, lr4_n = 3.1, lr5_n = 1.3, sub_n = 1.5,
+              exc_num = 0, exc_thick = 0)
 mo.DBRplot()
 mo.calc()
 mo.plot_reslt()
