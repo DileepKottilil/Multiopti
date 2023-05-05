174d1bbb53922b34be962538292431eddae92750
DileepKottilil <dileepkiitm@gmail.com>
2023-05-05 10:23:51 +0800

    init

--- test.py
+++ test.py
@@ -2,19 +2,6 @@ import multiopti as mop
 
 mo = mop.multiopti(1,3,200) #create an instance of class multiopt
 mo.ref_indx(source = 'theory')
-# mo.EM()
-# mo.DBR(Bragg = 555, mode = 10, air_n = 1, DBR_per_up = 1,DBR_per_bot = 1,
-#               lr1_n = 1, lr2_n = 1, cav_n = 1, lr4_n = 1, lr5_n = 1, sub_n = 1,
-#               exc_num = 0, exc_thick = 0)
-
-# mo.DBRplot()
-# mo.calc()
-# mo.plot_reslt()
-
-
-#mo.plot_0Deg()
-#mo.save_text()
-#mo.DBRplot()
 
 # P pol
 
